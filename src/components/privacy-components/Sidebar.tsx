e91b5592d21dc69c707f7e143fdf1e87d83abfc4
Tamara <tamaraelaye@gmail.com>
2025-01-08 01:48:04 +0100

    chore: update url on change of sections

--- src/components/privacy-components/Sidebar.tsx
+++ src/components/privacy-components/Sidebar.tsx
@@ -1,34 +1,41 @@
-import React from "react";
 
-interface SidebarProps {
-  setActivePolicy: (policy: "privacy" | "terms" | "refund") => void;
-}
 
-const PolicySidebar: React.FC<SidebarProps> = ({ setActivePolicy }) => {
-  const links: { name: string; value: "privacy" | "terms" | "refund" }[] = [
-    { name: "Privacy Policy", value: "privacy" },
-    { name: "Terms and Conditions", value: "terms" },
-    { name: "Refund Policy", value: "refund" },
+export default function PolicySidebar({
+  setActivePolicy,
+}: {
+  setActivePolicy: (policy: string) => void;
+}) {
+  const links = [
+    { name: "Privacy Policy", href: "#privacy" },
+    { name: "Terms and Conditions", href: "#terms" },
+    { name: "Refund Policy", href: "#refund" },
   ];
 
+  const handleClick = (href: string) => {
+    const hash = href.replace("#", "");
+    setActivePolicy(hash); // Update the active policy in the state
+  };
+
   return (
-    <aside className="h-screen p-6">
-      <nav>
+    <aside className="mx-auto px-4 md:items-center py-16 md:justify-between md:flex-row md:px-6 lg:px-8  h-screen p-4">
+      <nav className="mx-6">
         <ul className="space-y-4">
           {links.map((link) => (
-            <li key={link.value} className="border-b-2 py-4 border-gray-200">
-              <button
-                onClick={() => setActivePolicy(link.value)}
+            <li
+              key={link.name}
+              className="border-b-2 py-4 border-gray-200 max-w-64"
+            >
+              <a
+                href={link.href}
                 className="text-xl text-zinc-700 hover:text-primary"
+                onClick={() => handleClick(link.href)}
               >
                 {link.name}
-              </button>
+              </a>
             </li>
           ))}
         </ul>
       </nav>
     </aside>
   );
-};
-
-export default PolicySidebar;
+}
